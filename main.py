94fddcba93d5370dbede461106e44239f9eed3f1
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-09 11:57:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Fizzbuzz
+
+def fizzbuzz():
+    output = ""
+    for i in range(0, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+           print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+
+fizzbuzz()
