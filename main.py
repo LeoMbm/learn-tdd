5d43f87a2daae88a3041c651dff2257af0fd6de3
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-09 20:23:26 +0200

    roman number

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 # Fizzbuzz
 
-def fizzbuzz():
-    # output = ""
-    for i in range(0, 100):
-        if i % 3 == 0 and i % 5 == 0:
-            print('FizzBuzz')
-        elif i % 3 == 0:
-            print('Fizz')
-        elif i % 5 == 0:
-            print('Buzz')
-        else:
-            print(i)
+# def fizzbuzz():
+#     # output = ""
+#     for i in range(1, 100):
+#         if i % 3 == 0 and i % 5 == 0:
+#             print('FizzBuzz')
+#         elif i % 3 == 0:
+#             print('Fizz')
+#         elif i % 5 == 0:
+#             print('Buzz')
+#         else:
+#             print(i)
+#
+#
+# fizzbuzz()
 
 
-fizzbuzz()
+def int_to_roman(input):
+    if not 0 < input < 4000:
+        raise ValueError("Argument must be between 1 and 3999")
+    ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    nums = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    result = []
+    for i in range(len(ints)):
+        count = int(input / ints[i])
+        result.append(nums[i] * count)
+        input -= ints[i] * count
+    return ''.join(result)
+
+
+print(int_to_roman(3487))
