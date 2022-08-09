6d73ad918823a86a049aeef7e437ceab8e5aa43a
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-09 11:59:19 +0200

    exo1 done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Fizzbuzz
 
 def fizzbuzz():
-    output = ""
+    # output = ""
     for i in range(0, 100):
         if i % 3 == 0 and i % 5 == 0:
             print('FizzBuzz')
         elif i % 3 == 0:
-           print('Fizz')
+            print('Fizz')
         elif i % 5 == 0:
             print('Buzz')
         else:
